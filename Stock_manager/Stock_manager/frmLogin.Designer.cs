557b85c7a19ae4ae0a8c0072f9b3948c5b4e98db
Schneiter924 <raphael.schneiter@gmail.com>
2018-02-02 14:47:56 +0100

    création code partie graph
    modification du MCD et du MLD
    modification maquette
    modification journal de travail
    création du code la partie graphique

---
+++ Stock_manager/Stock_manager/frmLogin.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Stock_manager
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.cmdReset = new System.Windows.Forms.Button();
+            this.cmdAnnuler = new System.Windows.Forms.Button();
+            this.cmdConnexion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(12, 38);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(29, 13);
+            this.lblLogin.TabIndex = 0;
+            this.lblLogin.Text = "login";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(12, 73);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(71, 13);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Mot de passe";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(174, 35);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(182, 20);
+            this.txtLogin.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(174, 70);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(182, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // cmdReset
+            // 
+            this.cmdReset.Location = new System.Drawing.Point(134, 123);
+            this.cmdReset.Name = "cmdReset";
+            this.cmdReset.Size = new System.Drawing.Size(75, 23);
+            this.cmdReset.TabIndex = 4;
+            this.cmdReset.Text = "reset";
+            this.cmdReset.UseVisualStyleBackColor = true;
+            // 
+            // cmdAnnuler
+            // 
+            this.cmdAnnuler.Location = new System.Drawing.Point(215, 123);
+            this.cmdAnnuler.Name = "cmdAnnuler";
+            this.cmdAnnuler.Size = new System.Drawing.Size(75, 23);
+            this.cmdAnnuler.TabIndex = 5;
+            this.cmdAnnuler.Text = "Annuler";
+            this.cmdAnnuler.UseVisualStyleBackColor = true;
+            // 
+            // cmdConnexion
+            // 
+            this.cmdConnexion.Location = new System.Drawing.Point(296, 123);
+            this.cmdConnexion.Name = "cmdConnexion";
+            this.cmdConnexion.Size = new System.Drawing.Size(75, 23);
+            this.cmdConnexion.TabIndex = 6;
+            this.cmdConnexion.Text = "Connexion";
+            this.cmdConnexion.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 158);
+            this.Controls.Add(this.cmdConnexion);
+            this.Controls.Add(this.cmdAnnuler);
+            this.Controls.Add(this.cmdReset);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblLogin);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Connextion au serveur";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button cmdReset;
+        private System.Windows.Forms.Button cmdAnnuler;
+        private System.Windows.Forms.Button cmdConnexion;
+    }
+}
